9638620161adbffed23e971d06e455b57fc21b58
Snu Seam <siamahme766@gmail.com>
2026-04-21 09:27:44 +0600

    write schema and connect with database

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import app from ".";
+import { prisma } from "./lib/prisma";
+
+
+const PORT = process.env.PORT || 5000;
+
+async function main() {
+    try {
+        await prisma.$connect();
+        console.log("Database connected successfully.");
+
+        // Start your server or application logic here
+
+        app.get('/', (req,res)=>{
+            res.send("Hello World");
+        })
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+
+    }   catch (error) {
+        console.error("Error starting the server:", error);
+        await prisma.$disconnect();
+        process.exit(1);
+    }
+}
+
+main();
